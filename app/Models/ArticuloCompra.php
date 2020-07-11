c16a652b70b5d5ea704e2c432cc89c4988695a09
William <31410225+willguz@users.noreply.github.com>
2020-07-11 17:45:50 -0500

    cambios en la base de datos, para productos y compras

--- app/Models/ArticuloCompra.php
+++ app/Models/ArticuloCompra.php
@@ -8,10 +8,10 @@ class ArticuloCompra extends Model
 {
   protected $table = 'articulo_compra';
   // protected $primaryKey = 'Id_Articulo_Compra'; //por si la llave primaria tiene otro nombre
-  protected $fillable = ['id_articulo', 'id_compra', 'entregado', 'cantidad', 'descripcion', 'created_at', 'updated_at'];
+  protected $fillable = ['id_producto', 'id_compra', 'entregado', 'cantidad', 'descripcion', 'created_at', 'updated_at'];
 
   public function producto() {
-    return $this->belongsTo(Producto::class, 'id_articulo');
+    return $this->belongsTo(Producto::class, 'id_producto');
   }
   public function compra() {
     return $this->belongsTo(Compra::class, 'id_compra');
---
+++ database/migrations/2020_03_22_034144_categoria_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CategoriaProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categoria_productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('nombre');
+            $table->text('detalle');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categoria_productos');
+    }
+}
